bf839617d754c999f176c5e25ce4fd5cf3ae748d
Highlander Paiva <highlander.paiva@mercadolivre.com>
2024-09-19 10:55:43 -0300

    feat: added query timeouts

--- internal/data/movie.go
+++ internal/data/movie.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"context"
 	"database/sql"
 	"errors"
 	"time"
@@ -53,9 +54,12 @@ func (m MovieModel) Insert(movie *Movie) error {
 		RETURNING id, created_at
 	`
 
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
 	args := []any{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
 
-	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt)
+	return m.DB.QueryRowContext(ctx, query, args...).Scan(&movie.ID, &movie.CreatedAt)
 }
 
 func (m MovieModel) Get(id int64) (*Movie, error) {
@@ -71,7 +75,10 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
 
 	var movie Movie
 
-	if err := m.DB.QueryRow(query, id).Scan(
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	if err := m.DB.QueryRowContext(ctx, query, id).Scan(
 		&movie.ID,
 		&movie.Title,
 		&movie.Year,
@@ -108,7 +115,10 @@ func (m MovieModel) Update(movie *Movie) error {
 		movie.Version,
 	}
 
-	if err := m.DB.QueryRow(query, args...).Scan(&movie.Version); err != nil {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	if err := m.DB.QueryRowContext(ctx, query, args...).Scan(&movie.Version); err != nil {
 		switch {
 		case errors.Is(err, sql.ErrNoRows):
 			return ErrEditConflict
@@ -130,7 +140,10 @@ func (m MovieModel) Delete(id int64) error {
 		WHERE id = $1
 	`
 
-	res, err := m.DB.Exec(query, id)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	res, err := m.DB.ExecContext(ctx, query, id)
 	if err != nil {
 		return err
 	}
